9bae08a2b0bce64aeaee212bffd9abd0f809b2a0
uniquevitgen <vitgen1997@mail.ru>
2019-10-13 00:19:01 +0300

    lesson 9

--- src/main/java/ru/alishev/springcource/ClassicalMusic.java
+++ src/main/java/ru/alishev/springcource/ClassicalMusic.java
@@ -1,5 +1,8 @@
 package ru.alishev.springcource;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClassicalMusic implements Music {
     @Override
     public String getSong() {
